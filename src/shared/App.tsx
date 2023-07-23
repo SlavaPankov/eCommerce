87ccbee6e64be8ac09a0e2e6ff152dc6d1e945ac
Slava <slavapank94@yandex.ru>
2023-07-23 18:52:47 +0600

    feat: implement TestComponent & declare scss modules

--- src/shared/App.tsx
+++ src/shared/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import { TestComponent } from './TestComponent';
 
 export function App() {
   return (
-    <div>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut delectus, esse laboriosam minima
-      mollitia odit sequi similique. Culpa hic, magnam nemo quidem repellendus sapiente. At eveniet
-      excepturi necessitatibus quasi sequi.
-    </div>
+    <>
+      <TestComponent />
+    </>
   );
 }
---
+++ src/shared/TestComponent/TestComponent.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './testComponent.scss';
+
+export function TestComponent() {
+  return <h1 className={styles.heading}>Test comp</h1>;
+}
