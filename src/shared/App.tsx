84e210b45a784ef443ff50010c2d7509c1f99466
Slava <slavapank94@yandex.ru>
2023-07-23 18:35:45 +0600

    feat: implement base project settings

---
+++ src/shared/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function App() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut delectus, esse laboriosam minima
+      mollitia odit sequi similique. Culpa hic, magnam nemo quidem repellendus sapiente. At eveniet
+      excepturi necessitatibus quasi sequi.
+    </div>
+  );
+}
