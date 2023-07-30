b7a9fae81e8e07a95b30e8a09a074879f921b43c
Slava <slavapank94@yandex.ru>
2023-07-30 14:56:55 +0600

    feat: implement BaseInputField component

--- src/shared/App.tsx
+++ src/shared/App.tsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { BaseButton } from './BaseButton';
+import { BaseInputField } from './BaseTextInputField';
+
+interface INameInput {
+  value: string;
+  error: string;
+  isTouched: boolean;
+}
 
 export function App() {
+  const [nameInput, setNameInput] = useState<INameInput>({
+    value: '',
+    error: '',
+    isTouched: false
+  });
+
+  function handleOnChange(event: ChangeEvent<HTMLInputElement>) {
+    setNameInput({
+      ...nameInput,
+      value: event.target.value,
+      error: ''
+    });
+
+    if (event.target.value.length > 10) {
+      setNameInput({
+        ...nameInput,
+        value: event.target.value,
+        error: 'Слишком много буков!'
+      });
+    }
+  }
+
   return (
     <>
-      <BaseButton textContent="Получить" />
+      <BaseInputField
+        type="password"
+        name="name"
+        value={nameInput.value}
+        placeholder="Как вас зовут?"
+        onChange={handleOnChange}
+        error={nameInput.error}
+      />
+      <BaseButton textContent="Получить" onClick={() => console.log(nameInput.value)} />
     </>
   );
 }
