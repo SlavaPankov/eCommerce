a03beaf893dd91da11826442371009ac6c03b457
Slava <slavapank94@yandex.ru>
2023-08-10 08:09:55 +0600

    feat: implement base layout MainPage

---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Content } from '../../shared/Content';
+
+export function MainPage() {
+  return (
+    <Content>
+      <div>main</div>
+    </Content>
+  );
+}
