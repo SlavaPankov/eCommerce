e6549068e791999500fc9863db3458f2abcb172b
Slava <slavapank94@yandex.ru>
2023-08-11 17:08:02 +0600

    feat: implement CallbackForm UI

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -5,6 +5,7 @@ import { SpecialsListContainer } from '../../shared/SpecialsListContainer';
 import { HighRatingContainer } from '../../shared/HighRatingContainer';
 import { CommerceBanner } from '../../shared/CommerceBanner';
 import { TopCategoriesContainer } from '../../shared/TopCategoriesContainer';
+import { CallbackFormContainer } from '../../shared/CallbackFormContainer';
 
 export function MainPage() {
   return (
@@ -14,6 +15,7 @@ export function MainPage() {
       <HighRatingContainer />
       <CommerceBanner />
       <TopCategoriesContainer />
+      <CallbackFormContainer />
     </Content>
   );
 }
---
+++ src/shared/CallbackFormContainer/CallbackForm/CallbackForm.tsx
@@ -0,0 +1,55 @@
+import React, { ChangeEvent, useState } from 'react';
+import styles from './callbackForm.scss';
+import { BaseInputField } from '../../BaseTextInputField';
+import { BaseButton } from '../../BaseButton';
+
+interface IFormData {
+  name: string;
+  phone: string;
+  email: string;
+}
+
+const initialFormData: IFormData = {
+  name: '',
+  phone: '',
+  email: ''
+};
+
+export function CallbackForm() {
+  const [formData, setFormData] = useState<IFormData>(initialFormData);
+  const [formError] = useState<IFormData>(initialFormData);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  return (
+    <form className={styles.form}>
+      <BaseInputField
+        name="name"
+        value={formData.name}
+        placeholder="Ваше имя"
+        onChange={handleChange}
+        error={formError.name}
+      />
+      <BaseInputField
+        name="phone"
+        value={formData.phone}
+        placeholder="Ваш телефон"
+        onChange={handleChange}
+        error={formError.phone}
+      />
+      <BaseInputField
+        name="email"
+        value={formData.email}
+        placeholder="Ваш E-mail"
+        onChange={handleChange}
+        error={formError.email}
+      />
+      <BaseButton textContent="Отправить" />
+    </form>
+  );
+}
