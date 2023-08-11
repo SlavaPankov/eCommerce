96622ad6b25d17dc07e3930c751dd9d21de317b3
Hotimpulse <jz2@rambler.ru>
2023-08-11 16:29:09 +0300

    feat: adding the eye icon toggle for the password field

--- src/shared/LoginForm/LoginForm.tsx
+++ src/shared/LoginForm/LoginForm.tsx
@@ -4,15 +4,22 @@ import classNames from 'classnames';
 import styles from './loginForm.scss';
 import { BaseButton } from '../BaseButton';
 import { BaseInputField } from '../BaseTextInputField';
+import EyeIcon from '../Icons/EyeIcon/EyeIcon';
 
 export function LoginForm() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [emailError, setEmailError] = useState('');
   const [passwordError, setPasswordError] = useState('');
+  const [isEyeClicked, setIsEyeClicked] = useState(false);
 
-  const className = classNames('container', {
+  const containerClassName = classNames('container', {
     [`${styles.form}`]: true
+  });
+
+  const eyeIconClassName = classNames({
+    [`${styles.eye_icon}`]: true,
+    [`${styles.eye_icon_active}`]: isEyeClicked
   });
 
   const handleEmailValidation = (input: string) => {
@@ -21,7 +28,7 @@ export function LoginForm() {
     if (!emailTrimmed) {
       setEmailError('Введите e-mail');
     } else if (!emailRegex.test(emailTrimmed)) {
-      setEmailError('Неверный формат!');
+      setEmailError('Недопустимый формат!');
     } else {
       setEmailError('');
     }
@@ -33,7 +40,7 @@ export function LoginForm() {
     if (!passwordTrimmed) {
       setPasswordError('Введите пароль');
     } else if (!pwdRegex.test(passwordTrimmed)) {
-      setPasswordError(`Неверный формат! Пароль должен содержать не менее 8 символов, 
+      setPasswordError(`Недопустимый формат! Пароль должен содержать не менее 8 символов, 
       иметь хотя бы одну заглавную (A-Z) и строчную буквы (a-z), один номер и один спец. символ`);
     } else {
       setPasswordError('');
@@ -50,27 +57,38 @@ export function LoginForm() {
     handlePasswordValidation(event.target.value);
   };
 
+  const handleEyeClick = () => {
+    setIsEyeClicked(!isEyeClicked);
+  };
+
   return (
-    <form className={className}>
+    <form className={containerClassName}>
       <div className={styles.form__header_wrapper}>
         <h1 className={styles.form__header_login}>Вход</h1>
       </div>
-      <BaseInputField
-        name="e-mail"
-        value={email}
-        type="email"
-        placeholder="Ваш e-mail*"
-        onChange={handleChangeEmail}
-        error={emailError}
-      />
-      <BaseInputField
-        name="password"
-        value={password}
-        type="password"
-        placeholder="Ваш пароль*"
-        onChange={handleChangePassword}
-        error={passwordError}
-      />
+      <div className={styles.form__input_wrapper}>
+        <BaseInputField
+          name="e-mail"
+          value={email}
+          type="email"
+          placeholder="Ваш e-mail*"
+          onChange={handleChangeEmail}
+          error={emailError}
+        />
+        <div className={styles.form__password_wrapper}>
+          <BaseInputField
+            name="password"
+            value={password}
+            type="password"
+            placeholder="Ваш пароль*"
+            onChange={handleChangePassword}
+            error={passwordError}
+          />
+          <div className={eyeIconClassName} onClick={handleEyeClick}>
+            <EyeIcon />
+          </div>
+        </div>
+      </div>
 
       <BaseButton textContent="Войти" />
       <div className={styles.form__register_wrapper}>
