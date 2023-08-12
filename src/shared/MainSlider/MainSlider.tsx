94ece0426ea55dcfc4703816e4ae12bfb790d97c
Slava <slavapank94@yandex.ru>
2023-08-12 20:29:28 +0600

    feat: implement discountCode

--- src/shared/MainSlider/MainSlider.tsx
+++ src/shared/MainSlider/MainSlider.tsx
@@ -7,16 +7,35 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import styles from './mainSlider.scss';
 import { Modal } from '../Modal';
+import { useAppDispatch, useAppSelector } from '../../hooks/storeHooks';
+import { getDiscountCodeRequestAsync } from '../../store/discountCode/discountCodeSlice';
+import { ElephantIcon } from '../Icons';
+import { EBaseButtonMode } from '../../types/enums/EBaseButtonMode';
 
 export function MainSlider() {
-  const containerClassName = classNames('container', {
-    [`${styles.container}`]: true
-  });
+  const dispatch = useAppDispatch();
+  const { discountCode } = useAppSelector((state) => state);
+  const { token } = useAppSelector((state) => state.token.payload);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
   const handleClickDiscount = () => {
+    if (!token) {
+      return;
+    }
+
+    if (!discountCode.discountCode) {
+      dispatch(getDiscountCodeRequestAsync(token));
+    }
     setIsModalOpen(true);
   };
+
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText(discountCode.discountCode).then(() => console.log('copied'));
+  };
+
+  const containerClassName = classNames('container', {
+    [`${styles.container}`]: true
+  });
 
   return (
     <div className={styles.swiper__container}>
@@ -32,13 +51,18 @@ export function MainSlider() {
           <div className={containerClassName}>
             <h2 className={styles.heading}>Скидка 15% на первую покупку</h2>
             <BaseButton textContent="Получить" onClick={handleClickDiscount} />
-            {isModalOpen && (
+            {isModalOpen && !discountCode.loading && (
               <Modal onClose={() => setIsModalOpen(false)}>
-                <span>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam aut debitis
-                  facilis illum iure nostrum numquam perferendis quidem, repellat similique
-                  suscipit! Atque eligendi modi sapiente soluta tenetur voluptate? Ipsam!
-                </span>
+                <div className={styles.modal}>
+                  <ElephantIcon />
+                  <h4 className={styles.modal_title}>Ваш промокод:</h4>
+                  <div className={styles.modal_content}>{discountCode.discountCode}</div>
+                  <BaseButton
+                    textContent="Копировать"
+                    onClick={handleCopyClick}
+                    mode={EBaseButtonMode.secondary}
+                  />
+                </div>
               </Modal>
             )}
           </div>
--- src/store/store.ts
+++ src/store/store.ts
@@ -4,13 +4,15 @@ import categoriesReducer from './categories/categoriesSlice';
 import specialsReducer from './specials/specialSlice';
 import tokenReducer from './token/tokenSlice';
 import cartReducer from './cart/cartSlice';
+import dicountCodeReducer from './discountCode/discountCodeSlice';
 
 const reducerState = combineReducers({
   token: tokenReducer,
   products: productsReducer,
   categories: categoriesReducer,
   specials: specialsReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  discountCode: dicountCodeReducer
 });
 
 const store = configureStore({
