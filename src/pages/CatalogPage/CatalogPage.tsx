b15732beaffe15f5b083f3af92f27af468bcb7b4
Svystoslav Pankov <slavapank94@yandex.ru>
2023-08-28 12:10:14 +0600

    feat: implement breadcrumbs

--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import { Content } from '../../shared/Content';
 import { CategoriesContainer } from '../../shared/CategoriesContainer';
+import { ERoutes } from '../../types/enums/ERoutes';
 
 export function CatalogPage() {
+  const location = useLocation();
+
   return (
     <Content>
-      <CategoriesContainer />
+      {location.pathname === ERoutes.catalog ? <CategoriesContainer /> : <Outlet />}
     </Content>
   );
 }
