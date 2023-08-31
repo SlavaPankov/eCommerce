a3d145529e21977f50a7af1ebd52bd57f4d55a01
Hotimpulse <jz2@rambler.ru>
2023-08-31 21:35:39 +0300

    feat: adding attribute translations for the products characteristics

--- src/shared/Product/ProductCharacteristics/ProductCharacteristics.tsx
+++ src/shared/Product/ProductCharacteristics/ProductCharacteristics.tsx
@@ -6,12 +6,48 @@ interface IProductCharacteristicsProps {
   characteristics: Array<Attribute>;
 }
 
+type AttributeTranslations = {
+  [key: string]: string;
+};
+
 export function ProductCharacteristics({ characteristics }: IProductCharacteristicsProps) {
+  const attributeTranslations: AttributeTranslations = {
+    guarantee: 'Гарантия',
+    'vendor-code': 'Артикул',
+    length: 'Длина',
+    width: 'Ширина',
+    height: 'Высота',
+    depth: 'Глубина',
+    sku: 'Артикул',
+    'bed-width': 'Ширина кровати',
+    'bed-height': 'Высота кровати',
+    'bed-length': 'Длина кровати',
+    'bed-color-pick': 'Цвет кровати',
+    'bed-legs': 'Материал опор',
+    'bed-size': 'Спальное место',
+    delivery: 'Вариант доставки',
+    'sleepping-area': 'Спальное место',
+    'landing-height': 'Высота посадки',
+    'bed-mechanism': 'Подъёмный механизм кровати',
+    mechanism: 'Механизм',
+    'upholstery-type': 'Тип обивки',
+    color: 'Цвет',
+    'supports-material': 'Материал опор',
+    frame: 'Каркас',
+    'dresser-height': 'Высота комода',
+    'dresser-width': 'Ширина комода',
+    'dresser-depth': 'Глубина комода',
+    'dresser-color': 'Цвет',
+    'dresser-material': 'Материал корпуса',
+    'max-weight': 'Максимальная нагрузка',
+    'countertop-material': 'Материал столешницы'
+  };
+
   return (
     <ul className={styles.list}>
       {characteristics.map(({ name, value }, index) => (
         <li key={index} className={styles.item}>
-          <span className={styles.name}>{name}</span>
+          <span className={styles.name}>{attributeTranslations[name]}</span>
           {typeof value !== 'object' ? (
             <span className={styles.value}>{value}</span>
           ) : (
