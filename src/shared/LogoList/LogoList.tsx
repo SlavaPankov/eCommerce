6d190c1ca345c7c0e0a9e083f5a4951a8aa0dcb1
katie-soft <analyst.meo@gmail.com>
2023-09-13 12:59:55 +0300

    fix: add grid to logo list

--- src/shared/LogoList/LogoList.tsx
+++ src/shared/LogoList/LogoList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
+import { Link } from 'react-router-dom';
 import styles from './logoList.scss';
 import Agile from '../../assets/images/LogoAgile.png';
 import Figma from '../../assets/images/LogoFigma.png';
@@ -11,7 +12,17 @@ import ReduxLogo from '../../assets/images/LogoRedux.png';
 import ReactRouter from '../../assets/images/LogoReactRouter.png';
 import CommerceTools from '../../assets/images/LogoCommerceTools.jpg';
 
-const logos = [Agile, Jira, TS, ReactLogo, ReduxLogo, ReactRouter, Figma, Github, CommerceTools];
+const logos = [
+  { name: Agile, link: 'https://agilemanifesto.org/' },
+  { name: Jira, link: 'https://www.atlassian.com/software/jira' },
+  { name: TS, link: 'https://www.typescriptlang.org/' },
+  { name: ReactLogo, link: 'https://react.dev/' },
+  { name: ReduxLogo, link: 'https://redux.js.org/' },
+  { name: ReactRouter, link: 'https://reactrouter.com/en/main' },
+  { name: Figma, link: 'https://www.figma.com/' },
+  { name: Github, link: 'https://github.com/' },
+  { name: CommerceTools, link: 'https://docs.commercetools.com/' }
+];
 
 export function LogoList() {
   const listClassName = classNames('list list-reset', {
@@ -22,7 +33,9 @@ export function LogoList() {
     <ul className={listClassName}>
       {logos.map((logo, index) => (
         <li className={styles.item} key={index}>
-          <img className={styles.image} src={logo} />
+          <Link to={logo.link}>
+            <img className={styles.image} src={logo.name} />
+          </Link>
         </li>
       ))}
     </ul>
